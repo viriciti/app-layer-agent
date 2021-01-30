1f8ca24f0717f18520b330e70c5b896278adbceb
Simon Rijk <s.rijk@viriciti.com>
2021-01-30 23:08:40 +0100

    Be nicer in the entrypoint: let the container try 7 times with a timeout of 10 seconds.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-echo "Looking up listener host $APP_LAYER_MQTT_ENDPOINT with nslookup"
-nslookup $APP_LAYER_MQTT_ENDPOINT
+COUNTER=0
+while true; do
+  ((COUNTER+=1))
 
-EXIT_CODE=$?
+  echo "Count $COUNTER: Looking up listener host $APP_LAYER_MQTT_ENDPOINT with nslookup"
+  nslookup $APP_LAYER_MQTT_ENDPOINT
 
-if [ $EXIT_CODE -eq 0 ]; then
-  echo "Domain $APP_LAYER_MQTT_ENDPOINT resolvable! Exit code was $EXIT_CODE. Starting application..."
-  /usr/local/bin/node /app/build/main.js
-else
-  echo "Domain $APP_LAYER_MQTT_ENDPOINT NOT resolvable: exit code was $EXIT_CODE. Exiting application..."
-fi
+  EXIT_CODE=$?
+
+  if [ $EXIT_CODE -eq 0 ]; then
+    echo "Count $COUNTER: Domain $APP_LAYER_MQTT_ENDPOINT resolvable! Exit code was $EXIT_CODE. Starting application..."
+    break
+  else
+    echo "Count $COUNTER: Domain $APP_LAYER_MQTT_ENDPOINT NOT resolvable: exit code was $EXIT_CODE. Waiting 10 seconds..."
+    sleep 10
+
+    if [[ $COUNTER -gt 6 ]]; then
+      echo "Count $COUNTER: Tried nslookup 7 times. No luck giving up!"
+      exit 0
+    fi
+  fi
+done
+
+/usr/local/bin/node /app/build/main.js
